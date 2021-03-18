8154db97b74817d2f58137c8e69290f9af06e16f
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-03-18 18:22:55 +0100

    Updates

---
+++ precache-manifest.7561d6be8b465f77a51e1c6ebe1d1217.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1c2807db3a8f1bec4b0f56f3e75188",
+    "url": "/first-portfolio/index.html"
+  },
+  {
+    "revision": "315ebad8aa2e30d69542",
+    "url": "/first-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "12f7a5cdcfdf84f8b2be",
+    "url": "/first-portfolio/static/css/main.d07654e2.chunk.css"
+  },
+  {
+    "revision": "315ebad8aa2e30d69542",
+    "url": "/first-portfolio/static/js/2.792860bc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/first-portfolio/static/js/2.792860bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f7a5cdcfdf84f8b2be",
+    "url": "/first-portfolio/static/js/main.75955f05.chunk.js"
+  },
+  {
+    "revision": "78f05e2df91fbf2b451f",
+    "url": "/first-portfolio/static/js/runtime-main.7706dea9.js"
+  },
+  {
+    "revision": "39265304212f8c63504310af0228ce33",
+    "url": "/first-portfolio/static/media/myProfile.39265304.jpg"
+  },
+  {
+    "revision": "314dca2a841488c35ba65825476787fc",
+    "url": "/first-portfolio/static/media/p0.314dca2a.png"
+  },
+  {
+    "revision": "09d8fb0249c760e84a96661952530707",
+    "url": "/first-portfolio/static/media/p1.09d8fb02.png"
+  },
+  {
+    "revision": "32927daa32370544c45a69ca1d6cc0c5",
+    "url": "/first-portfolio/static/media/p1.32927daa.png"
+  },
+  {
+    "revision": "a24b6063081952229081610460f13427",
+    "url": "/first-portfolio/static/media/p1.a24b6063.png"
+  },
+  {
+    "revision": "242eea72e5b00855e92ddbd090547f0f",
+    "url": "/first-portfolio/static/media/p2.242eea72.png"
+  },
+  {
+    "revision": "3e3d36e08e4c32851edde42e01d9cd58",
+    "url": "/first-portfolio/static/media/p2.3e3d36e0.png"
+  },
+  {
+    "revision": "8365fbf2a32043e0112ed18cdb0e7317",
+    "url": "/first-portfolio/static/media/p2.8365fbf2.png"
+  },
+  {
+    "revision": "9ae20af7f4c7a7d20e5cafe3140fd7e5",
+    "url": "/first-portfolio/static/media/p3.9ae20af7.png"
+  },
+  {
+    "revision": "a4eb421fd46bda62fff20e5b22a3979b",
+    "url": "/first-portfolio/static/media/p3.a4eb421f.png"
+  }
+]);
